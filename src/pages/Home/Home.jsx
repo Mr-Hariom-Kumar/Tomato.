25d69ee20c3b27668eff3d56dbea3922ef786563
Mr-Hariom-Kumar <766hariwork@gmail.com>
2025-08-23 11:58:54 +0530

    done responsive setup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,14 +4,16 @@ import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import { useState } from 'react'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDowload/AppDownload'
 const Home = () => {
   const [category,setCategory]=useState("All");
   return (
-    <div>
+    <div className='home'>
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
          {/* point of diff */}
      <FoodDisplay category={category} />
+     <AppDownload></AppDownload>
     </div>
   )
 }
